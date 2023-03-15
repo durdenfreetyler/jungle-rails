18ab9b79dd9202fea856d50bab2330c346f76812
Abe <abrahamrodgers@gmail.com>
2023-03-14 19:18:41 -0700

    Cypress home page test complete

---
+++ cypress/integration/1-getting-started/product.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+context("product", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
+});
