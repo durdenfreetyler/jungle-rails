946f79b29f74bcdc8722345aaa4acdfc81900ea9
Abe <abrahamrodgers@gmail.com>
2023-03-15 10:45:48 -0700

    Final Cypress testing

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -19,10 +19,10 @@ class CartsController < ApplicationController
 
   private
 
-  def modify_cart_delta(product_id, delta)
-    cart[product_id] = (cart[product_id] || 0) + delta
-    cart.delete(product_id) if cart[product_id] < 1
-    update_cart cart
-  end
+def modify_cart_delta(product_id, delta)
+  cart[product_id] = (cart[product_id] || 0) + delta
+  cart.delete(product_id) if cart[product_id] <= 0
+  update_cart cart
+end
 
 end
--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,17 +1,19 @@
 /// <reference types="cypress" />
 
-context("product", () => {
+describe("jungle", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
-
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
   });
-
-
+  it(".click() - click on a DOM element", () => {
+    // https://on.cypress.io/click
+    cy.contains("My Cart (0)");
+    cy.get("article .btn").first().click({ force: true });
+    cy.contains("My Cart (1)");
+  });
 });
